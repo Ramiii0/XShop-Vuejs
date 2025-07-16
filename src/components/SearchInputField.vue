6c88fc80ff18597d2739e7359e3c864f8f090cee
rami <ramiiya02r@gmail.com>
2025-07-16 21:54:20 +0300

    Add SearchInputField component with loading state functionality

---
+++ src/components/SearchInputField.vue
@@ -0,0 +1,26 @@
+<template>
+      <v-text-field
+        :loading="loading"
+        append-inner-icon="mdi-magnify"
+        density="compact"
+        label="Search templates"
+        variant="solo"
+        hide-details
+        single-line
+        @click:append-inner="onClick"
+      ></v-text-field>
+</template>
+<script setup>
+  import { ref } from 'vue'
+
+  const loaded = ref(false)
+  const loading = ref(false)
+
+  function onClick () {
+    loading.value = true
+    setTimeout(() => {
+      loading.value = false
+      loaded.value = true
+    }, 2000)
+  }
+</script>
