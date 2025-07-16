01ff727535d357cb487cc84c4af20d730d14eaf3
rami <ramiiya02r@gmail.com>
2025-07-16 21:53:38 +0300

    Add route names constant and update router configuration

---
+++ src/constants/routeNames.ts
@@ -0,0 +1,13 @@
+const APP_ROUTE_NAMES = Object.freeze({
+  HOME: 'home',
+  SIGN_IN: 'sign-in',
+  SIGN_UP: 'sign-up',
+  PRODUCTS_LIST: 'products-list',
+  PRODUCT_CREATE: 'product-create',
+  PRODUCT_UPDATE: 'product-update',
+  CONTACT_US: 'contact-us',
+  ACCESS_DENIED: 'access-denied',
+  NOT_FOUND: 'not-found',
+})
+
+export { APP_ROUTE_NAMES }
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,15 +7,17 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
+import Home from '@/pages/Home.vue'
+import { APP_ROUTE_NAMES } from '@/constants/routeNames'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes : [
-    // {
-    //   path: '/',
-    //   name: APP_ROUTE_NAMES.HOME,
-    //   component: Home,
-    // }
+    {
+      path: '/',
+      name: APP_ROUTE_NAMES.HOME,
+      component: Home,
+    }
   ]
 })
 
